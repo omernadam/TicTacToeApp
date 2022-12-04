de3f7b60ed6d93e121b3b983aa3a0f667d0ff3df
Erez Leviim <erezleviim@gmail.com>
2022-12-04 21:41:09 +0200

    Add winning functions

--- app/src/main/java/com/example/tictactoeapp/MainActivity.java
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.java
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void draw() {
-        textWinnerView.setText("Draw!");
+        textWinnerView.setText("Draw");
     }
 
 }
